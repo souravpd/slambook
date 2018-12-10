ca3f47260a2eab33428752c30d4445ec5921c553
Sourav Prasad <souravpd007@gmail.com>
2018-12-10 23:58:29 +0530

    Added the DB Class Functionality

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php 
 require_once("core/init.php");
-?>
+
+$db = DB::getInstance();
+$db = $db->get("users",array("username",array("username","=","alex")));
+if($db!=false){
+    if($db->count){
+        echo "OK";
+    }
+    else{
+        echo " NO";
+    }
+}
+else{
+    echo "NOT EXECUTED";
+}
